bbb0e4031e40f8722f1e2aa8f8f5afe15611bbbd
Muhammad Talha Waqas <81808768+MT-Waqas@users.noreply.github.com>
2024-05-07 02:12:00 +0500

    Update Program.cs

--- githubLearning/Program.cs
+++ githubLearning/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Owasoftpvtltd...!");
+
+public static class Test
+{
+    static Test()
+    {
+        Console.WriteLine();
+    }
+}
